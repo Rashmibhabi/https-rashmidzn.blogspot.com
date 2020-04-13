99b98039ad47c3544e17077755e9b4b58dc5d446
Rashmi Design <35373381+Rashmibhabi@users.noreply.github.com>
2020-04-13 08:46:59 +0530

    Delete Rashmi Official.js

--- Rashmi Official.js
+++
@@ -1,1 +0,0 @@
-function rm(e){var a=document.getElementById(e);imgtag="",img=a.getElementsByTagName("img"),imgtag=1<=img.length?'<img src="'+img[0].src+'" />':"",a.innerHTML=imgtag+'<div class="itemCaption"><div class="capContent"><h3 class="post-title entry-title" itemprop="name"><a href="'+y+'">'+x+'</a></h3><a class="capPreview image-modal" href="'+y+'">See Details</a></div></div>'}
